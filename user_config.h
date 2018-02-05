29ca6d28847092fbccf751fa46ee520d00f75a90
Sthopeless <hughbrian@hotmail.com>
2018-02-05 17:44:12 +0100

    WIFI & MQTT Broker & Topics Configurations

---
+++ user_config.h
@@ -0,0 +1,15 @@
+/************************* WIFI CONFIGURATION **********************************/
+#define wifi_ssid "WiFi-SSID"
+#define wifi_password "WiFi-Password"
+/************************* MQTT BROKER CONFIGURATION ***************************/
+#define mqtt_server "mqtt.broker.server.ip"
+#define mqtt_username "MQTT-Username"
+#define mqtt_password "MQTT-Password"
+#define mqtt_port 1883
+/************************* MQTT TOPICS CONFIGURATION ***************************/
+#define mqtt_command "/MQTTKeypad/commmand/"
+#define mqtt_state "/MQTTKeypad/state/"
+
+#define cmnd_on "On"
+#define cmnd_off "Off"
+/*************************** CONFIGURATION ***********************************/
